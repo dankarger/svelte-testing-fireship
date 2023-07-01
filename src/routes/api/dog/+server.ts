cc32173daec940c87b031c2ca6408bd8a4954cf4
dankarger <karger.dan@gmail.com>
2023-07-01 14:16:50 +0300

    add boat page + actions

--- src/routes/api/dog/+server.ts
+++ src/routes/api/dog/+server.ts
@@ -1,16 +1,22 @@
+import { error, json } from "@sveltejs/kit";
 import type { RequestEvent, RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async (e: RequestEvent) => {
   e.cookies;
   e.params;
   e.request.body;
-  e.fetch("SomeURL");
+  // e.fetch("SomeURL");
 
   return new Response();
 };
 
-export const POST: RequestHandler = async () => {
-  return new Response();
+export const POST: RequestHandler = async (e: RequestEvent) => {
+  let user = { admin: true };
+
+  if (!user.admin) {
+    throw error(401, "Unauthorized");
+  }
+  return json({ name: "dog " });
 };
 
 export const DELETE: RequestHandler = async () => {
